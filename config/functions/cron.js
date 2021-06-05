3492171ccd2030b503ac0a44c2870ca162ba2afa
jinzhunan <47770525+jinzhunan@users.noreply.github.com>
2021-06-05 12:03:25 -0400

    first commit

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -15,8 +15,45 @@ module.exports = {
    * Simple example.
    * Every monday at 1am.
    */
-  '*/1 * * * *': () => {
-    console.log('consle every 1 min')
+  '* * 6 * * *': async () => {
+    console.log('consle every 5 min')
+    const d = new Date()
+    // const year = d.getFullYear()
+    // const month = d.getMonth() + 1
+    // const day = d.getDate()
+
+    // console.log(`${year}/${month}/${day}`)
+
     // strapi.config.functions.docker();
+
+    const daily = await strapi.api.test.services.test.find(
+      // {createdAt: new Date()}
+      );
+      const daily2 = daily.filter(item =>{
+        const isYear = item.createdAt.getFullYear() === d.getFullYear()
+        const isMonth = item.createdAt.getMonth() + 1 === d.getMonth() + 1
+        const isDay = item.createdAt.getDate() === d.getDate()
+        // console.log(isYear)
+        // console.log(isMonth)
+        // console.log(isDay)
+        if(isYear && isMonth && isDay){
+          return item
+        }
+      })
+    // console.log(daily2)
+      const contect = daily2.map((item, index) => (`
+        <h3>visitor${index + 1}:</h3>
+        country: ${item.visitor_country}<br/>
+        city: ${item.visitor_city}<br/>
+        ip: ${item.visitor_ip}<br/>
+        postal: ${item.visitor_postal}<br/>
+        count: ${item.day_count}<br/>
+      `)).toString()
+      console.log(contect)
+    await strapi.plugins['email'].services.email.send({
+      to:"nanjinzhu666@gmail.com",
+      subject: 'daily visitors',
+      html: contect
+    })
   }
 };
--- config/server.js
+++ config/server.js
@@ -7,5 +7,5 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', '54161cfdf412334a3045a2e31b9c517e'),
     },
   },
-  cron: { enabled: false},
+  cron: { enabled: true},
 });
