ff40f3fa160a69affb2e60f546ec54a35cf040c7
jinzhunan <47770525+jinzhunan@users.noreply.github.com>
2021-06-12 06:18:18 -0400

    first commit

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -16,36 +16,53 @@ module.exports = {
    * Every monday at 1am.
    */
   //  '59 59 23 * * *' every day 23:59:59
-  '59 59 23 * * *': async () => {
-    console.log('consle every 5 min')
+  '10 10 */1 * * *': async () => {
     const d = new Date()
 
     const daily = await strapi.api.test.services.test.find(
       // {createdAt: new Date()}
       );
       const daily2 = daily.filter(item =>{
-        const isYear = item.createdAt.getFullYear() === d.getFullYear()
-        const isMonth = item.createdAt.getMonth() + 1 === d.getMonth() + 1
-        const isDay = item.createdAt.getDate() === d.getDate()
+
+        const currentYear = d.getFullYear()
+        const currentMon = d.getMonth() + 1
+        let currentDay = d.getDate()
+        if(d.getDate() === 1){
+          if(currentMon === 1 || currentMon === 3 || currentMon === 5 || currentMon === 7 || currentMon === 8 || currentMon === 10 || currentMon === 12){
+            currentDay = 31
+          }else if(currentMon === 4 || currentMon === 6 || currentMon === 9 || currentMon === 11 ){
+            currentDay = 30
+          }else if(currentMon === 2){
+            currentDay = 28
+          }
+        }else{
+          currentDay = currentDay - 1
+        }
+
+        const isYear = item.createdAt.getFullYear() === currentYear
+        const isMonth = item.createdAt.getMonth() + 1 === currentMon
+        const isDay = item.createdAt.getDate() === currentDay
 
         if(isYear && isMonth && isDay){
           return item
         }
       })
-    // console.log(daily2)
-      const contect = daily2.map((item, index) => (`
+      console.log(daily2)
+
+      let contect = daily2.map((item, index) => (`
         <h3>visitor${index + 1}:</h3>
         country: ${item.visitor_country}<br/>
         city: ${item.visitor_city}<br/>
         ip: ${item.visitor_ip}<br/>
         postal: ${item.visitor_postal}<br/>
         count: ${item.day_count}<br/>
+        date: ${currentYear}-${currentMon}-${currentDay}<br/>
       `)).toString()
       console.log(contect)
-    await strapi.plugins['email'].services.email.send({
-      to:"nanjinzhu666@gmail.com",
-      subject: 'daily visitors',
-      html: contect
-    })
+    // await strapi.plugins['email'].services.email.send({
+    //   to:"nanjinzhu666@gmail.com",
+    //   subject: 'daily visitors',
+    //   html: contect
+    // })
   }
 };
